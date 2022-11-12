6f946d93681e7db2906cd05e32b16fadfa1c0dca
Noorkhokhar <34125851+noorkhokhar99@users.noreply.github.com>
2022-11-12 06:24:02 +0500

    Add files via upload

---
+++ application/config.py
@@ -0,0 +1,6 @@
+class Config:
+    DEBUG = True
+    IMAGE_UPLOADS = "application/static/uploads/"
+    MODEL = "application/static/models/model_v1_meat.h5"
+    SESSION_COOKIE_SECURE = False
+    SECRET_KEY = "secretkeysuperkey"
